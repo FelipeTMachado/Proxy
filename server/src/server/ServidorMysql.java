ac06d26b26df9111279a31dda417f3cdfd9e24a0
canpse <lucas.campestrini@gmail.com>
2024-07-25 18:41:06 -0300

    00005 - adicioando proxy provisorio e interfaces de oberservavel

---
+++ server/src/server/ServidorMysql.java
@@ -0,0 +1,16 @@
+package server;
+
+import java.util.Map;
+
+/**
+ *
+ * @author lucas campestrini <lucas.campestrini@gmail.com>
+ */
+public class ServidorMysql implements Servidor{
+
+    @Override
+    public Map<String, String> requisitar() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
