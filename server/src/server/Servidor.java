7c61da4de97edd19c7b0bfec13526a91d09f14d9
canpse <lucas.campestrini@gmail.com>
2024-07-25 18:40:41 -0300

    00005 - adicioando proxy provisorio e interfaces de oberservavel

---
+++ server/src/server/Servidor.java
@@ -0,0 +1,14 @@
+package server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author lucas campestrini <lucas.campestrini@gmail.com>
+ */
+interface Servidor {
+
+    Map<String, String> requisitar();
+    
+}
